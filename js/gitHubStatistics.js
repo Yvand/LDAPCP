705ac25a9fc227c8b392e104bd7a9c37787b81a7
Yvan Duhamel <yvandev@outlook.fr>
2017-12-22 15:29:32 +0100

    Implement statistics on ldapcp.com (#36)
    * Implementing JSONP to workaround same origin policy
    
    * Updating XHR to use jsonp
    
    * Adding statistics
    
    * Implementing statistics
    
    * Implemented statistics

--- js/gitHubStatistics.js
+++ js/gitHubStatistics.js
@@ -1,35 +1,53 @@
 /// <reference path="../node_modules/@types/jquery/index.d.ts" />
 // npm install --save @types/jquery
+/*function parseGitHubStatisticsResponse (p1) {
+    console.log('Got callback.');
+}*/
 var GitHubStatistics;
 (function (GitHubStatistics) {
     class LDAPCPStats {
         constructor() {
             this.url = "http://ldapcp-functions.azurewebsites.net/api/GetRepoStats";
+            //url: string = "http://jsfiddle.net/echo/jsonp/";
             this.authZKey = "Xs141m0QqIUrDBfecYvdhOf0cJJ8sA2LygLgkVcKmTdwIU5ELx1OCg==";
         }
         getLatestStat() {
-            console.log('Hello World');
+            console.log("Sending query to " + this.url);
             $.ajax({
                 method: "GET",
                 crossDomain: true,
-                contentType: "application/json; charset=utf-8",
-                //dataType: "json",
-                url: this.url + "?code=" + this.authZKey + "&callback=?",
+                data: { code: this.authZKey },
+                dataType: "jsonp",
+                jsonpCallback: "parseGitHubStatisticsResponse",
+                url: this.url,
                 success: function (responseData, textStatus, jqXHR) {
                     console.log("Data received");
                     var value = responseData;
                     console.log(value);
                 },
                 error: function (responseData, textStatus, errorThrown) {
-                    console.log('Request failed.');
+                    console.log("Request failed: " + errorThrown);
                 }
             });
         }
+        static decodeJSONResponse(json) {
+            var obj = Object.assign({}, json, {
+                created: new Date(json.DateStatCreatedUTC)
+            });
+            return obj;
+        }
+        static parseGitHubStatisticsResponse(data) {
+            var result = GitHubStatistics.LDAPCPStats.decodeJSONResponse(data);
+            $("#TotalDownloadCount").text(result.TotalDownloadCount);
+            $("#LatestReleaseDownloadCount").text(result.LatestReleaseDownloadCount);
+            $("#LatestReleaseTagName").text(result.LatestReleaseTagName);
+            $("#LatestAssetUrl").attr("href", result.LatestAssetUrl);
+        }
+        ;
     }
     GitHubStatistics.LDAPCPStats = LDAPCPStats;
 })(GitHubStatistics || (GitHubStatistics = {}));
 $(document).ready(function () {
     let stats = new GitHubStatistics.LDAPCPStats();
     let result = stats.getLatestStat();
-    //$("#status")[0].innerHTML = message;
 });
--- ts_src/gitHubStatistics.ts
+++ ts_src/gitHubStatistics.ts
@@ -1,33 +1,66 @@
 /// <reference path="../node_modules/@types/jquery/index.d.ts" />
 // npm install --save @types/jquery
-namespace GitHubStatistics {
+
+/*function parseGitHubStatisticsResponse (p1) {
+    console.log('Got callback.');    
+}*/
+
+namespace GitHubStatistics {    
+    export interface RepoStatsJSON {
+        DateStatCreatedUTC: string;
+        Repository: string;
+        LatestAssetUrl: string;
+        LatestReleaseCreationDate: string;
+        LatestReleaseTagName: string;
+        LatestReleaseDownloadCount: number;
+        AllReleasesDownloadCount: number;
+        TotalDownloadCount: number;
+    }
+
     export class LDAPCPStats {
         url: string = "http://ldapcp-functions.azurewebsites.net/api/GetRepoStats";
+        //url: string = "http://jsfiddle.net/echo/jsonp/";
         authZKey: string = "Xs141m0QqIUrDBfecYvdhOf0cJJ8sA2LygLgkVcKmTdwIU5ELx1OCg==";
         getLatestStat() {
-            console.log('Hello World');
+            console.log("Sending query to " + this.url);
             
             $.ajax({
                 method: "GET",
                 crossDomain: true,
-                contentType: "application/json; charset=utf-8",
-                //dataType: "json",
-                url: this.url + "?code=" + this.authZKey + "&callback=?",
+                data: {code: this.authZKey},
+                dataType: "jsonp",
+                jsonpCallback: "parseGitHubStatisticsResponse",
+                url: this.url,
                 success: function(responseData, textStatus, jqXHR) {
                     console.log("Data received");                
                     var value = responseData;
                     console.log(value);
                 },
                 error: function (responseData, textStatus, errorThrown) {
-                    console.log('Request failed.');
+                    console.log("Request failed: " + errorThrown);
                 }
             });
         }
+
+        static decodeJSONResponse(json: GitHubStatistics.RepoStatsJSON) {
+            var obj = Object.assign({}, json, {
+                created: new Date(json.DateStatCreatedUTC)
+            });
+            return obj;
+        }
+
+        static parseGitHubStatisticsResponse (data) {
+            var result =  GitHubStatistics.LDAPCPStats.decodeJSONResponse(data);
+            $("#TotalDownloadCount").text(result.TotalDownloadCount);
+            $("#LatestReleaseDownloadCount").text(result.LatestReleaseDownloadCount);
+            $("#LatestReleaseTagName").text(result.LatestReleaseTagName);
+            $("#LatestAssetUrl").attr("href", result.LatestAssetUrl)
+        };
     }
 }
 
 $(document).ready(function () {
     let stats = new GitHubStatistics.LDAPCPStats();
     let result = stats.getLatestStat()
-    //$("#status")[0].innerHTML = message;
 });
+
