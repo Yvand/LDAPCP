7e9a071333a26462aea06903fb5ff9a1470e27a7
Michael Rees Pullen <mrpullen@outlook.com>
2018-12-17 21:17:45 +0000

    Updated DEV branch so I can get this in as a PR to YVand repo correctly.

---
+++ LDAPCP/ClaimEntity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ldapcp
+{
+    public class ClaimEntity
+    {
+        public string ClaimType
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        public string ValueType
+        {
+            get;
+            set;
+        }
+    }
+}
