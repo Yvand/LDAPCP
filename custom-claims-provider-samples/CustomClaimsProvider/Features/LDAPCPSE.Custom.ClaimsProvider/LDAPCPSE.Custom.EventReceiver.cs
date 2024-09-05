f2362f109f953dc044c45f6b2142f332b1680161
Yvan Duhamel <yvandev@outlook.fr>
2024-09-05 16:41:44 +0200

    work

--- custom-claims-provider-samples/CustomClaimsProvider/Features/LDAPCPSE.Custom.ClaimsProvider/LDAPCPSE.Custom.EventReceiver.cs
+++ custom-claims-provider-samples/CustomClaimsProvider/Features/LDAPCPSE.Custom.ClaimsProvider/LDAPCPSE.Custom.EventReceiver.cs
@@ -1,7 +1,9 @@
 using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.Administration.Claims;
 using System;
 using System.Runtime.InteropServices;
-using System.Security.Permissions;
+using Yvand.LdapClaimsProvider.Logging;
 
 namespace CustomClaimsProvider.Features
 {
@@ -13,39 +15,59 @@ namespace CustomClaimsProvider.Features
     /// </remarks>
 
     [Guid("8a0189bf-2e90-48b0-85f7-f639b42e3ef8")]
-    public class LDAPCPSECustomEventReceiver : SPFeatureReceiver
+    public class LDAPCPSECustomEventReceiver : SPClaimProviderFeatureReceiver
     {
-        // Uncomment the method below to handle the event raised after a feature has been activated.
+        public override string ClaimProviderAssembly => typeof(LDAPCPSE_Custom).Assembly.FullName;
 
-        //public override void FeatureActivated(SPFeatureReceiverProperties properties)
-        //{
-        //}
+        public override string ClaimProviderDescription => LDAPCPSE_Custom.ClaimsProviderName;
 
+        public override string ClaimProviderDisplayName => LDAPCPSE_Custom.ClaimsProviderName;
 
-        // Uncomment the method below to handle the event raised before a feature is deactivated.
+        public override string ClaimProviderType => typeof(LDAPCPSE_Custom).FullName;
 
-        //public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
-        //{
-        //}
+        public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        {
+            ExecBaseFeatureActivated(properties);
+        }
 
+        private void ExecBaseFeatureActivated(Microsoft.SharePoint.SPFeatureReceiverProperties properties)
+        {
+            // Wrapper function for base FeatureActivated. 
+            // Used because base keywork can lead to unverifiable code inside lambda expression
+            base.FeatureActivated(properties);
+            SPSecurity.RunWithElevatedPrivileges((SPSecurity.CodeToRunElevated)delegate ()
+            {
+                try
+                {
+                    Logger svc = Logger.Local;
+                    Logger.Log($"[{LDAPCPSE_Custom.ClaimsProviderName}] Activating farm-scoped feature for claims provider \"{LDAPCPSE_Custom.ClaimsProviderName}\"", TraceSeverity.High, EventSeverity.Information, TraceCategory.Configuration);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException((string)LDAPCPSE_Custom.ClaimsProviderName, $"activating farm-scoped feature for claims provider \"{LDAPCPSE_Custom.ClaimsProviderName}\"", TraceCategory.Configuration, ex);
+                }
+            });
+        }
 
-        // Uncomment the method below to handle the event raised after a feature has been installed.
+        public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
+        {
+        }
 
-        //public override void FeatureInstalled(SPFeatureReceiverProperties properties)
-        //{
-        //}
+        public override void FeatureDeactivating(SPFeatureReceiverProperties properties)
+        {
+            SPSecurity.RunWithElevatedPrivileges((SPSecurity.CodeToRunElevated)delegate ()
+            {
+                try
+                {
+                    Logger.Log($"[{LDAPCPSE_Custom.ClaimsProviderName}] Deactivating farm-scoped feature for claims provider \"{LDAPCPSE_Custom.ClaimsProviderName}\": Removing claims provider from the farm (but not its configuration)", TraceSeverity.High, EventSeverity.Information, TraceCategory.Configuration);
+                    base.RemoveClaimProvider((string)LDAPCPSE_Custom.ClaimsProviderName);
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException((string)LDAPCPSE_Custom.ClaimsProviderName, $"deactivating farm-scoped feature for claims provider \"{LDAPCPSE_Custom.ClaimsProviderName}\"", TraceCategory.Configuration, ex);
+                }
+            });
+        }
 
-
-        // Uncomment the method below to handle the event raised before a feature is uninstalled.
-
-        //public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
-        //{
-        //}
-
-        // Uncomment the method below to handle the event raised when a feature is upgrading.
-
-        //public override void FeatureUpgrading(SPFeatureReceiverProperties properties, string upgradeActionName, System.Collections.Generic.IDictionary<string, string> parameters)
-        //{
-        //}
     }
 }
