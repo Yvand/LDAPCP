9ecf52b14a8883a25fd6dc44bc224db4aa8a13a2
Yvan Duhamel <yvandev@outlook.fr>
2026-05-06 16:14:35 +0200

    Update NuGet dependencies (#253)
    * fix compiler errors
    
    * downgrade NUnit to 4.5.1 due to ThreadAbortException with 4.6.0

--- Yvand.LDAPCPSE.Tests/ClaimsProviderTestsBase.cs
+++ Yvand.LDAPCPSE.Tests/ClaimsProviderTestsBase.cs
@@ -1,13 +1,10 @@
-﻿using Microsoft.Office.Audit.Schema;
-using Microsoft.SharePoint.Administration.Claims;
-using Microsoft.SharePoint.BusinessData.Administration;
+﻿using Microsoft.SharePoint.Administration.Claims;
 using Microsoft.SharePoint.WebControls;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Linq;
 using System.Security.Claims;
